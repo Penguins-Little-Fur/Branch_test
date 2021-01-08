bfebd2cfa949d396323af3e391031d0f537ac0b0
企鹅的小毛毛 <536486513@qq.com>
2021-01-08 10:39:15 +0100

    Update divide function
    Add a divide function and call

--- branch.c
+++ branch.c
@@ -12,12 +12,18 @@ int mul(int a, int b){
   return a * b;
 }
 
+int div(int a, int b){
+  return a / b;
+}
+
 int main(){
   c = add(5,2);
   d = sub(5,2);
   g = mul(5,2);
+  h = div(6,2);
   printf(c);
   printf(d);
   printf(g);
+  printf(h);
   return 0;
 }
